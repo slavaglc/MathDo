c63b8af7a96f20512e23765bb250b28475a77924
Maxim Goryachkin <maxgoryachkin@mail.ru>
2022-01-06 11:29:44 +0300

    Initial Commit

---
+++ MathDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathDo
+//
+//  Created by Максим Горячкин on 06.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
