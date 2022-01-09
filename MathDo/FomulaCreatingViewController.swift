7d2666c28ab06516a55e4c14b17cf1d067bef67f
slavaglc <47712523+slavaglc@users.noreply.github.com>
2022-01-09 19:50:11 +0300

    created FormulaVC

---
+++ MathDo/FomulaCreatingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FomulaCreatingViewController.swift
+//  MathDo
+//
+//  Created by Вячеслав Макаров on 09.01.2022.
+//
+
+import UIKit
+
+
+ final class FomulaCreatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
